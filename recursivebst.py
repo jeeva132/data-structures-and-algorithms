0c11ce6553e1fdb524cd95b1cd1a675afead7af2
JEEVAN S <111651080+jeeva132@users.noreply.github.com>
2023-08-14 16:50:43 +0530

    updated

--- recursivebst.py
+++ recursivebst.py
@@ -28,6 +28,22 @@ class BinarySearchTree:
                     temp.right = new_node
                     return True
                 temp = temp.right
+
+     def r_insert(self,value):
+        if self.root == None:
+            self.root = Node(value)
+            
+        self.__r_insert(self.root,value)
+    
+    def __r_insert(self,current_node,value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left,value)
+            
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right,value)
+        return current_node
         
     def r_contains(self,value):
         return self.__r_contains(self.root,value)
