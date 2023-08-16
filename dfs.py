f6eeffb85cf56f5d9eb52364f2baf3cd4c5b5efe
JEEVAN S <111651080+jeeva132@users.noreply.github.com>
2023-08-16 23:00:38 +0530

    added with post order

--- dfs.py
+++ dfs.py
@@ -65,9 +65,22 @@ class BinarySearchTree:
                 traverse(current_node.left)
             if current_node.right is not None:
                 traverse(current_node.right)
-                
         traverse(self.root)
         return results
+
+    def dfs_post_order(self):
+        results = []
+        
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+            
+        traverse(self.root)
+        return results
+
 
 
 
@@ -80,16 +93,13 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.dfs_pre_order())
+print(my_tree.dfs_post_order())
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    [47, 21, 18, 27, 76, 52, 82]
+    [18, 27, 21, 52, 82, 76, 47]
 
  """
-
-                
-
