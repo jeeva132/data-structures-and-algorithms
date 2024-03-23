85b7afc19046e98ce2e4704fa33013e36674fdac
JEEVAN S <chuppijeevan@gmail.com>
2024-03-23 19:47:19 +0530

    Create facttrailingzero172.py

---
+++ facttrailingzero172.py
@@ -0,0 +1,12 @@
+class Solution:
+    
+    def trailingZeroes(self, n: int) -> int:
+        
+        i  = 1
+        count = 0
+        while n//pow(5,i):
+            count +=n//pow(5,i)
+            i +=1
+        return count
+
+        
