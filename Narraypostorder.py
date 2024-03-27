0c9e708319333239bb54ba7001d90ff3a8511428
JEEVAN S <chuppijeevan@gmail.com>
2024-03-27 22:48:46 +0530

    Create Narraypostorder.py

---
+++ Narraypostorder.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res = []
+
+        def dfs(cur,res):
+            if cur is None:
+                return 
+            
+            for child in cur.children:
+                dfs(child,res)
+            res.append(cur.val)
+        dfs(root,res)
+        return res
+        
