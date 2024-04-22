f8c8cb65d5dbd4f98b0d6e1a8775314e2b7068ec
JEEVAN S <chuppijeevan@gmail.com>
2024-04-22 20:19:27 +0530

    Update balancedbinarytree.py

--- balancedbinarytree.py
+++ balancedbinarytree.py
@@ -12,7 +12,7 @@ class Solution:
                 return [True,0]
             
             left,right = dfs(root.left),dfs(root.right)
-            balanced = (left[0] and right[0] and abs(left[1]-right[1]<=1))
+            balanced = (left[0] and right[0] and abs(left[1]-right[1])<=1)
             return [balanced,1+max(right[1],left[1])]
         
         return dfs(root)[0]
