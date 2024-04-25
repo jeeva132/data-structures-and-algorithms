2a433bd8359c42bf9034d987a8d4a6eca0c68ea6
JEEVAN S <chuppijeevan@gmail.com>
2024-04-25 21:27:41 +0530

    Create valueafteroperation.py

---
+++ valueafteroperation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        total = 0
+        for i in operations:
+            if i == 'X++' or i == '++X':
+                total +=1
+            else:
+                total -=1 
+        
+        return total
