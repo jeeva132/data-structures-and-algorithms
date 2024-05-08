0d1f7c673897874524f70387fa147b9abe29fef6
JEEVAN S <chuppijeevan@gmail.com>
2024-05-08 22:41:06 +0530

    Update add.py

--- add.py
+++ add.py
@@ -8,4 +8,4 @@ def add_numbers(x, y):
 num1 = 10
 num2 = 15
 sum_result = add_numbers(num1, num2)
-print(f"Sum of {num1} and {num2} is: {sum_result}")
+print(f"Sum of {num1} and {num2} is : {sum_result}")
