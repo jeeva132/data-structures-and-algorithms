068c20a482b449603357b865a74fb733097d6a97
JEEVAN S <chuppijeevan@gmail.com>
2024-07-16 22:12:42 +0530

    Create sumofvaluesofbitsatkindex.py

---
+++ sumofvaluesofbitsatkindex.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        res = 0
+
+        for i in range(len(nums)):
+            if bin(i)[2:].count('1') == k:
+                res += nums[i]
+        
+        return res
+        
